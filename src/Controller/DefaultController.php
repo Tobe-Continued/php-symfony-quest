829c53652ad381fc5d75ede357ea98f21b86338c
Tobe_Continued <do.you.continue@gmail.com>
2020-07-29 11:28:04 +0200

    03 - Symfony : Création de la page d’accueil

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route(name="app_")
+ */
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        return $this->render('default/index.html.twig', [
+            'title' => 'Bienvenue sur Wild Series',
+        ]);
+    }
+}
